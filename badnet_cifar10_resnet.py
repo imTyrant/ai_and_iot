89b1902f81714bffafcbbaefd827eb9705e040b7
imTyrant <cd2235823998@gmail.com>
2020-01-05 19:49:40 +0800

    update

--- badnet_cifar10_resnet.py
+++ badnet_cifar10_resnet.py
@@ -7,6 +7,7 @@ import torch.functional as F
 import torch.optim as O
 from torchvision import transforms, datasets
 from poisoning_data import PoisonedCIFAR10, bomb_pattern_cifar
+from resnet_4_cifar10 import ResNet50
 
 import os
 import sys
@@ -16,11 +17,19 @@ BATCH_SIZE = 20
 NUM_WORKER = 8
 EPOCHS = 350
 
+'''
+# Preprocess 4 ImageNet based ResNet
 preprocess = transforms.Compose([
     transforms.Resize(256),
     transforms.CenterCrop(224),
     transforms.ToTensor(),
     transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+])
+'''
+# Preprocess 4 CIFAR10 based ResNet
+preprocess = transforms.Compose([
+    transforms.ToTensor(),
+    transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010)),
 ])
 
 benign_trainset = datasets.CIFAR10('.data', train=True, transform=preprocess)
@@ -29,6 +38,9 @@ benign_train_data = DataLoader(benign_trainset, batch_size=BATCH_SIZE, shuffle=T
 benign_testset = datasets.CIFAR10('.data', train=False, transform=preprocess)
 benign_test_data = DataLoader(benign_testset, batch_size=BATCH_SIZE, num_workers=NUM_WORKER, shuffle=True)
 
+def get_resnet50_mode_for_cifar10():
+    # return torchvision.models.resnet50(num_classes=10)
+    return ResNet50()
 
 def lazy_init(model):
     optimizer = O.SGD(model.parameters(), lr=0.1, momentum=0.9)
@@ -68,7 +80,7 @@ def train_poisoned_data(epsilon):
     poisoned_testset = PoisonedCIFAR10('.data', bomb_pattern_cifar, train=False, epsilon=1, only_pd=True, target=5, transform=preprocess,)
     poison_test_data = DataLoader(poisoned_testset, batch_size=BATCH_SIZE, shuffle=True, num_workers=NUM_WORKER)
 
-    model = torchvision.models.resnet50(num_classes=10).to(DEVICE)
+    model = get_resnet50_mode_for_cifar10().to(DEVICE)
     
     trainer = Trainer(model, poison_train_data, device=DEVICE, hp=lazy_init(model))
 
@@ -88,16 +100,16 @@ def train_poisoned_data(epsilon):
 
 
 if __name__ == "__main__":
-    epsilon = -1
-    for each in map(lambda x: (x.split('=')[0], x.split('=')[1]), sys.argv[1:]):
-        cmd, value = each
-        print(each)
-        if cmd == '--gpu':
-            DEVICE = value
-        if cmd == '--eps':
-            epsilon = float(value)
-    if epsilon == -1:
-        epsilon = 0
-    train_poisoned_data(epsilon)
+    # epsilon = -1
+    # for each in map(lambda x: (x.split('=')[0], x.split('=')[1]), sys.argv[1:]):
+    #     cmd, value = each
+    #     print(each)
+    #     if cmd == '--gpu':
+    #         DEVICE = value
+    #     if cmd == '--eps':
+    #         epsilon = float(value)
+    # if epsilon == -1:
+    #     epsilon = 0
+    # train_poisoned_data(epsilon)
 
-    # train_benign_resnet50()
+    train_benign_resnet50()
